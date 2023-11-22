2cc15919d5382fe551d3863bb12019ad2e30c10d
Christina <nana.yee0211@yahoo.com>
2023-11-22 06:33:41 -0800

    Add new message-box component and image - to display message box after complete an action - to display newly added image in message box

---
+++ src/app/message-box/message-box.component.ts
@@ -0,0 +1,35 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ToDoItem } from '../to-do-list/to-do-list.model';
+
+@Component({
+  selector: 'app-message-box',
+  templateUrl: './message-box.component.html',
+  styleUrls: ['./message-box.component.css']
+})
+export class MessageBoxComponent {
+  public toDoItem: ToDoItem = {};
+  public message: string = "";
+  public textToBold: string = "";
+  public hasImage: boolean = false;
+  public imageSrc: string = "";
+  public buttonText: string = "";
+
+  constructor(public dialogRef: MatDialogRef<MessageBoxComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any){
+    this.toDoItem = this.data.toDoItem;
+    this.message = this.data.message;
+    this.textToBold = this.data.textToBold;
+    this.hasImage = this.data.hasImage;
+    this.imageSrc = this.data.imageSrc;
+    this.buttonText = this.data.buttonText;
+  }
+
+  ngOnInit(){
+    
+  }
+
+  close(){
+    this.dialogRef.close(this.toDoItem);
+  }
+}
